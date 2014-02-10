e3d930fbb0f16548ce0a6a18a7ff7bd7d1e7dde6
Matt Bertino <mattbertino@gmail.com>
2014-02-10 14:52:00 -0500

    Styling and foundation for button

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,9 +3,34 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('We have todo App', function () {
+        describe('when I click the add button', function () {
+            it('should add a todo to an array', function () {
+
+            });
+            it('should add a todo template to the todo list div', function () {
+
+            });
+            it('should make the landingpage disappear', function () {
+
+            });
+        });
+        describe('when I click the done button', function () {
+            it('should change the done value of that object to true', function () {
+
+            });
+            it('should be added to the completed array', function () {
+
+            });
+        });
+        describe('when I click the remove button', function () {
+            it('should remove a todo from an array', function () {
+
+            });
+            it('should remove a todo template from the todo list div', function () {
+
+            });
+            it('should pop up a "are you sure" box', function () {
 
             });
         });
