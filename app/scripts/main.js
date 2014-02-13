2adbc808fb72caeeba5e30f6e72e7dd2704d1c02
Matt Bertino <mattbertino@gmail.com>
2014-02-13 12:01:10 -0500

    finished all functionality up to edit and counts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,53 @@
 var todoData =[
 {
-	description: 'Go to town',
-	done: false,
-	id: _.uniqueId(),
+  description: 'Shower',
+  done: false,
+  id: _.uniqueId(),
 },
 {
-	description: 'Eat lunch',
-	done: false,
-	id: _.uniqueId(),
+  description: 'Drink Coffee',
+  done: false,
+  id: _.uniqueId(),
 },
 {
-	description: 'Force push to origin master',
-	done: false,
-	id: _.uniqueId(),
+  description: 'Eat Breakfast',
+  done: false,
+  id: _.uniqueId(),
 },
 {
-	description: 'Scratch that last one',
-	done: false,
-	id: _.uniqueId(),
+  description: 'Go to town',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Do WORK!!',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Eat lunch',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Force push to origin master',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Scratch that last one',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Eat Dinner',
+  done: false,
+  id: _.uniqueId(),
+},
+{
+  description: 'Brush teeth',
+  done: false,
+  id: _.uniqueId(),
 },
 ]
 
@@ -30,6 +60,20 @@ $(document).ready(function(){
   _.each(todoData, function(item, index){
     $('.js-todo-list').prepend(todoTemplate(item))
   });
+
+updateCount()
+
+function updateCount(){
+  var allCount = todoData.length
+  var activeArray = _.where(todoData, {done: false})
+  var activeCount = activeArray.length
+  var completedArray = _.where(todoData, {done: true})
+  var completedCount = completedArray.length
+
+  $('.js-all-badge').empty().html(allCount)
+  $('.js-active-badge').empty().html(activeCount)
+  $('.js-completed-badge').empty().html(completedCount)
+}
 
 // click events
   $('.js-row').on('click', '.add-button', function(){
@@ -45,7 +89,7 @@ $(document).ready(function(){
 
     var renderedTodo = (todoTemplate(todo))
     $('.js-todo-list').prepend(renderedTodo)
-
+    updateCount()
   });
 
   $('.js-row').on('click','.remove', function(){
@@ -57,13 +101,12 @@ $(document).ready(function(){
     })
 
     $(this).parent().parent().remove();
+    updateCount()
   });
 
   $('.js-row').on('click','.completed', function(){
-  	// this removes the item from the array.
+  	// this marks an item as completed the array.
   	var parentId = $(this).parent().parent().attr('id').split('-')[1];
-  	
-  	console.log(parentId)
 
   	_.each(todoData, function(item, index){ 
       if (item.id == parentId) {
@@ -71,20 +114,38 @@ $(document).ready(function(){
       };
     })
     $(this).parent().parent().css('background-color','#D6D6D6')
+    updateCount()
   });
 
-///////////////////////////////////////
-// This is where the problem starts....
-///////////////////////////////////////
+  $('.js-row').on('dblclick','.description', function(){
+      $(this).siblings('.js-edit-input').show();
+      $(this).siblings().children('.js-edit').focus();
+  });
+  
+  // this hides edit and impacts the array as well as the div with the correct edit
+  $('.js-row').on('blur','.js-edit', function(){
+      var parentId = $(this).parent().parent().attr('id').split('-')[1];
+      var editInput = $(this).val()
+
+      _.each(todoData, function(item, index){ 
+        if (item.id === parentId) {
+          item.description = editInput;
+        };
+      });
+
+      $(this).parent().siblings('.description').empty().html(editInput)
+      
+      $(this).parent('.js-edit-input').hide();
+  });
 
   $('.folder-list').on('click','.js-all-todos', function(){
   	console.log('hey there')
   	$('.note-item').remove();
 
   	_.each(todoData, function(item, index){
-	    $('.js-todo-list').prepend(todoTemplate(item))
-	  });
-
+      $('.js-todo-list').prepend(todoTemplate(item))
+    });
+    updateCount()
   });
  
 	$('.folder-list').on('click','.js-active-todos', function(){
@@ -96,7 +157,8 @@ $(document).ready(function(){
 
 		_.each(activeTodoData, function(item, index){
 	    $('.js-todo-list').prepend(todoTemplate(item))
-	  });
+    });
+    updateCount()
 	});
 
 	$('.folder-list').on('click','.js-completed-todos', function(){
@@ -108,8 +170,8 @@ $(document).ready(function(){
 
 		_.each(completedTodoData, function(item, index){
 	    $('.js-todo-list').prepend(todoTemplate(item))
-	  });
-		$('.note-item').css('background-color','#D6D6D6')
+    });
+    updateCount()
 	});
 })
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -5,31 +5,32 @@
 
     describe('We have todo App', function () {
         describe('when I click the add button', function () {
-            it('should add a todo to an array', function () {
-                $('.js-landing-todo-input').val('beer')
+            it('should add a todo to a div', function () {
+                var todoData =[]
+                var todoTemplate = _.template($('.todo-template').text())
 
-                $('.js-landing-todo-add').click()
+                $('.js-todo-input').val('apples');
 
-                var firstTodoText = $('.todo-list').first().children('.description').text();
+                $('.js-landing-todo-add').click();
+
+                var firstTodoText = $('.todo-list').first().children().first().children('.description').children().text();
                 
-                expect(firstTodoText).to.contain('beer')
+                expect(firstTodoText).to.contain("apples");
             });
         });
         describe('when I click the done button', function () {
             it('should change the done value of that object to true', function () {
-                $('')
-
-                expect()
-            });
-            it('should be added to the completed array', function () {
 
             });
         });
         describe('when I click the remove button', function () {
             it('should remove a todo from an array', function () {
+                var todoData =[]
+                $('.js-todo-input').val('beer');
 
-            });
-            it('should remove a todo template from the todo list div', function () {
+                $('.js-landing-todo-add').click();
+
+                $('.remove').click()
 
             });
             it('should pop up a "are you sure" box', function () {
